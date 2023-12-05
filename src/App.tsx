143453d26968a6a2b4cf323f613309579e40e0d1
smart-builder <ringroseleekevin@gmail.com>
2023-12-05 21:17:17 +0000

    integrate react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className='App'>
+
     </div>
   );
 }
